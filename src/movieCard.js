6a8adabf26fcbedc542fe8d3c9a81d0a4a085dd5
mujammil28 <64261184+mujammil28@users.noreply.github.com>
2025-01-23 00:41:29 +0530

    movie card change dynamically using state property

--- src/movieCard.js
+++ src/movieCard.js
@@ -3,23 +3,83 @@ import { Component } from "react";
 
 export default class MovieCard extends Component{
 
+  constructor(){
+    super();
+
+    this.state={
+      img:"https://i.pinimg.com/736x/21/d6/21/21d62106e83e8bddfa41024dfc195356.jpg",
+        title:"The Avengers",
+        plot:"It is action and animation dilled movie.",
+        price:"199",
+        rating:"8.9",
+        stars:1,
+    }
+  }
+
+  addStar=()=>{
+      console.log(this.state);
+
+     // Form-1 // this.setState({
+
+      //   stars:this.state.stars+0.5,
+
+      // })
+      //form-2
+
+      this.setState((prevState)=>{
+          
+          return {
+            stars:prevState.stars+0.5,
+          }
+      })
+
+  }
+  
+      removeStar=()=>{
+
+       this.setState((prevState)=>{
+        if(prevState.stars>0){return {
+               stars:prevState.stars-0.5,
+            }
+          }
+        })
+      }
 
 render(){
+  const {img,title, plot, price, rating, stars}=this.state;
+
     return (
       <div className="main">
         <div className="movie-card">
         <div className="left">
-            <img alt="poster-image" src="https://i.pinimg.com/736x/21/d6/21/21d62106e83e8bddfa41024dfc195356.jpg"/>
+            <img alt="poster-image" src={img}/>
         </div>
 
         <div className="right">
-        <div className="title">The avengers</div>
-        <div className="plot">It is action and animation dilled movie.</div>
-        <div className="price"> Rs.199/-</div>
+        <div className="title">{title}</div>
+        <div className="plot">{plot}</div>
+        <div className="price"> Rs.{price}/-</div>
 
         <div className="footer">
-        <div className="rating">8.9</div>
-        <div className="stars"> *****</div>
+        <div className="rating">{rating}</div>
+        <div className="star-dis">
+        
+        <img className="str-btn"  onClick={this.removeStar}
+         src="https://cdn-icons-png.flaticon.com/128/1828/1828901.png"/>
+        
+        &nbsp;&nbsp; 
+        
+        <img className="stars" 
+        src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png"/>
+      &nbsp;&nbsp;
+      
+      <img className="str-btn"  onClick={this.addStar}
+      src="https://cdn-icons-png.flaticon.com/128/3524/3524388.png"/>
+
+      &nbsp;&nbsp;&nbsp;<span>{stars}</span>
+        </div>
+    
+       
         <button className="favourite-btn">Favorite</button>
         <button className="cart-btn">Add to cart</button>
         </div>
